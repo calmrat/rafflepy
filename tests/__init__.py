fc860dba68d498019f8a33b19b6ab44745b2b845
etrott <etrott@redhat.com>
2015-09-17 11:44:08 +0200

    Add csv_load and filters tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Author: Eduard Trott
-# @Date:   2015-09-17 11:41:18
+# @Date:   2015-09-17 11:40:01
 # @Email:  etrott@redhat.com
 # @Last modified by:   etrott
-# @Last Modified time: 2015-09-17 11:41:37
+# @Last Modified time: 2015-09-17 11:41:50
--- tests/test_rafflepy.py
+++ tests/test_rafflepy.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Author: Eduard Trott
-# @Date:   2015-09-16 15:30:33
+# @Date:   2015-09-17 11:41:28
 # @Email:  etrott@redhat.com
 # @Last modified by:   etrott
-# @Last Modified time: 2015-09-16 16:10:59
+# @Last Modified time: 2015-09-17 11:42:44
+
 
 from __future__ import unicode_literals, absolute_import
 
@@ -39,18 +40,18 @@ def test_gspread_load():
     filepath = ''.join(
         random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
 
-    # Uploading test-spreadsheet
+    # Uploading df to test-spreadsheet
     d2g.upload(df_upload, filepath)
     credentials = get_credentials()
     file_id = get_file_id(credentials, filepath)
 
-    # loading via file_id
+    # Downloading via file_id
     uri = file_id
     pool = raffle.input_gload(uri)
 
     assert all(pool == df_upload['Username'].values)
 
-    # loading certain column
+    # Downloading certain column
     column = 'color'
     pool = raffle.input_gload(uri, column)
 
@@ -58,3 +59,49 @@ def test_gspread_load():
 
     # Clear created file from drive
     delete_file(credentials, file_id)
+
+
+def test_csv_load():
+    import os
+
+    import pandas as pd
+
+    from rafflepy import raffle
+
+    df_upload = pd.DataFrame({'color': ['b', 'w', 'p', 'm'],
+                              'Username': ['John', 'Nicolos', 'Katy', 'Mike'],
+                              'Other': [1, 2, 3, 4]})
+
+    uri = 'sample.csv'
+
+    # Uploading df to csv
+    df_upload.to_csv(uri, encoding='utf-8')
+
+    # Downloading with default column
+    pool = raffle.input_load(uri)
+
+    assert all(pool == df_upload['Username'].values)
+
+    # Downloading certain column
+    column = 'color'
+    pool = raffle.input_load(uri, column)
+
+    assert all(pool == df_upload['color'].values)
+
+    # Clear created file
+    os.remove(uri)
+
+
+@pytest.mark.parametrize('execution_number', range(10))
+def test_exclude(execution_number):
+    import string
+    import random
+
+    from rafflepy import raffle
+
+    pool = [''.join([random.choice(string.ascii_uppercase + string.digits)
+                     for _ in range(random.randint(2, 10))])
+            for _ in range(random.randint(2, 10))]
+    exclude = [random.choice(range(0, len(pool)))
+               for _ in range(random.randint(1, len(pool)))]
+    assert set(pool) - set(exclude) == raffle.input_filter(pool, exclude)
